ef7bcf328a6dc6f9f24450757c828dbe4f99429c
苏震 <leslie43@sina.com>
2018-12-29 16:37:32 +0800

    markdown文件添加

--- src/components/RepositoryOptions/index.js
+++ src/components/RepositoryOptions/index.js
@@ -5,16 +5,25 @@ const repository = getRepository();
 export default class RepositoryOptions extends Component{
     constructor(props){
         super(props)
-        console.info(this.props);
+        // console.info(this.props);
     }
-    // 方法,可调用父组件方法(emit)
+    
+    // 方法,可调用父组件方法(emit) - 声明式_方法_works 🌼🌼🌼
+    // onChangeChild(val){
+    //     console.info(val);
+    //     this.props.onChangeParent(val);
+    // }
+
+    // 方法,可调用父组件方法(emit) - 箭头函数_方法_works 🎃🎃🎃
     onChangeChild = (value) => {
         this.props.onChangeParent(value);
     }
 
     render(){
         return(
-            <Checkbox.Group style={{ width: '100%' }} onChange={this.onChangeChild}>
+            // <Checkbox.Group style={{ width: '100%' }} onChange={(value)=>this.onChangeChild(value)}> 声明式_方法_works 🌼🌼🌼
+            // 箭头函数_方法_works 🎃🎃🎃
+            <Checkbox.Group style={{ width: '100%' }} onChange={this.onChangeChild}> 
                 <Row>{
                     repository.map((col) =>
                     <Col span={8} key={col.value}>
