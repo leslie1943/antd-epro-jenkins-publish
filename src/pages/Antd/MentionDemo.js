0ee7f6d04ff5c58d173ace2fdf3a311c74dd9048
苏震 <leslie43@sina.com>
2019-01-20 23:40:17 +0800

    Update MentionDemo.js

--- src/pages/Antd/MentionDemo.js
+++ src/pages/Antd/MentionDemo.js
@@ -3,6 +3,8 @@ import { connect } from 'dva';
 import moment from 'moment';
 import {Form,Mention} from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import layout from "@/utils/layout";
+
 const FormItem = Form.Item;
 
 // toContentState: 把字符串转成 ContentState
@@ -31,21 +33,35 @@ class MentionDemo extends Component{
     }
 
     render(){
+        const { form: { getFieldDecorator , getFieldValue}} = this.props;
         return(
             <PageHeaderWrapper title="Antd demo - Mention" content="mention">
-                <Mention style={{width:'100%'}}
-                
-                defaultValue={toContentState('@afc163')}
-                
-                suggestions={this.state.suggestionList}
-                // defaultSuggestions={this.state.suggestionList}
-                // onSelect={(suggestion)=>this.selectHandle(suggestion)} =====> ✨✨✨✨ 传统定义的方法 ✨✨✨✨
-                // onChange={(v) => this.changeHandle(v)}  =====> ✨✨✨✨ 传统定义的方法 ✨✨✨✨
-                onChange={this.changeHandle}
-                onSelect={this.selectHandle}
-                placement="top"
-                >
-                </Mention>
+                <Form>
+                    {/* 每个FormItem分为两部分:
+                    1: 装饰器部分: name, rules,initialValue 等options
+                    2: 元素部分: 
+                        <Form.Item>
+                            {
+                                1: getFieldDecorator('xxxxxx',{options})
+                                2: (<Element onChange={this.changeHandle}></Element>)
+                            }
+                        </Form.Item>
+                    */}
+                    <Form.Item {...layout.formItemLayout} label="Name">
+                       {getFieldDecorator('name',{
+                           rules:[{required:true,message:'sssss'}],
+                           initialValue: toContentState('@afc163')
+                       })(<Mention style={{width:'100%'}}
+                       suggestions={this.state.suggestionList}
+                       // defaultSuggestions={this.state.suggestionList}
+                       // onSelect={(suggestion)=>this.selectHandle(suggestion)} =====> ✨✨✨✨ 传统定义的方法 ✨✨✨✨
+                       // onChange={(v) => this.changeHandle(v)}  =====> ✨✨✨✨ 传统定义的方法 ✨✨✨✨
+                       onChange={this.changeHandle}
+                       onSelect={this.selectHandle}
+                       placement="top"
+                   ></Mention>)}
+                    </Form.Item>
+                </Form>
             </PageHeaderWrapper>
         )
     }
