cfe02c86c3fbb012f1627a0cd145a729fc3c6896
苏震 <suzhen@viewhigh.com>
2020-03-11 16:15:50 +0800

    Hooks

--- src/pages/Demo/Hook.js
+++ src/pages/Demo/Hook.js
@@ -1,15 +1,25 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'antd';
-
 function HookDemo() {
+  // 参数: state,更新state的方法
   const [count, setCount] = useState(0)
+  const [minus, setMiuns] = useState(0)
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  })
+  const handleClick = () => {
+    setCount(count + 1)
+    setMiuns(minus - 1)
+  }
   return (
     <div>
       <p>you clicked {count} times</p>
-      <Button type="primary" onClick={() => setCount(count + 1)}>Add</Button>
+      <p>you clicked {minus} times</p>
+      <Button type="primary"
+        onClick={handleClick}>Add</Button>
     </div>
   )
 }
-
 export default HookDemo;
